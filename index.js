57de4cc7d9033c0a7374216d968ba2315d15a0ed
Kevin Foster <kevin.foster.uk@gmail.com>
2017-09-26 15:02:32 +0100

    Expose libs via main script

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict'
+
+module.exports = require('./lib/server');
+module.exports.Server = require('./lib/server');
+module.exports.RemoteObject = require('./lib/remote-object');
+module.exports.Repo = require('mzen').Repo;
+module.exports.Service = require('mzen').Service;
+module.exports.ModelManager = require('mzen').ModelManager;
