d44010b8595d39ec274cc5d5e498ea626959e379
Kevin Foster <kevin.foster.uk@gmail.com>
2019-03-20 12:34:45 +0800

    Expose ServerRepo and ServerService

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import Server from './server';
 
 export * from './server';
 export * from './remote-object';
+export * from './repo';
+export * from './service';
 export * from './acl/role-assessor';
 export * from 'mzen';
 
