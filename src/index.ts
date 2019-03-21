72fc5ed0b7d254d0feca24f79a9b4985a8ba5d37
Kevin Foster <kevin.foster.uk@gmail.com>
2019-03-21 12:33:44 +0800

    Expose error classes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import Server from './server';
 
-export * from './server';
+export * from './acl/role-assessor';
+export * from './api-config';
+export * from './error';
 export * from './remote-object';
 export * from './repo';
+export * from './server';
 export * from './service';
-export * from './api-config';
-export * from './acl/role-assessor';
 export * from 'mzen';
 
 export default Server;
