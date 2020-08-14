c9ad260309a5db05407ed79f3f661e14b110cebf
Kevin Foster <kevin.foster.uk@gmail.com>
2020-08-14 14:17:28 +0100

    Remove ServerService and ServerRepo use mzen Service and Repo instead

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,7 @@ export * from './acl/role-assessor';
 export * from './api-config';
 export * from './error';
 export * from './remote-object';
-export * from './repo';
 export * from './server';
-export * from './service';
 export * from 'mzen';
 
 export default Server;
